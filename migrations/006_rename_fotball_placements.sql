c53cbfa0aa0867172cc17e8e442b6e0f2d656885
thor kinstad <thor.jarle.kinstad@felleskjopet.no>
2026-06-29 13:40:41 +0200

    Rename ballbinge_placements to activity_fotball_placements

---
+++ migrations/006_rename_fotball_placements.sql
@@ -0,0 +1,4 @@
+-- Rename ballbinge_placements to activity_fotball_placements
+-- Run this migration in Supabase SQL Editor
+
+ALTER TABLE IF EXISTS ballbinge_placements RENAME TO activity_fotball_placements;
